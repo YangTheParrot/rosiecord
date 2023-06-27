f17f3a1db04ef0ee00bfc9d99067020c93e5ea72
YangTheParrot <81785867+YangTheParrot@users.noreply.github.com>
2023-06-27 03:13:42 -0400

    [184] stable update

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 export default {
-    IPA_FETCH_LINK: "https://cdn.discordapp.com/attachments/986937851042213888/1120979519382245457/Discord_183.0.ipa",
+    IPA_FETCH_LINK: "https://cdn.discordapp.com/attachments/986937851042213888/1123145675580911681/Discord_184.0.ipa",
     ENMITY_LOADER: "Enmity.Debug.deb",
     GET_PATCH_TYPE<T, V>(predicate: Function, inputArg: V, truePredicate: T, falsePredicate: T, fallback: T): T {
         const out: boolean = predicate(inputArg);
